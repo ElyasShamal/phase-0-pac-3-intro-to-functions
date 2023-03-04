d5e25950f6d3bfa41107075c7282f838f95f3a45
Elyas <Elyas.shamal2@gmail.com>
2023-03-04 13:38:04 -0500

    add some functions

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Follow along with the examples here
+
+
+function sayHelloToGuada() {
+    console.log("hello, Guadalupa!");
+}
+
+sayHelloToGuada();
+
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+}
+
+sayHelloToLiz();
+
+
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+}
+
+sayHelloToSamip();
+
+
+a = 10 ;
+b = 20 ;
+
+function add(){
+    console.log(a+b);
+}
+
+add();
+
+
+
+c = 100;
+d = 10;
+
+function div(){
+    console.log(c/d);
+}
+
+div();
